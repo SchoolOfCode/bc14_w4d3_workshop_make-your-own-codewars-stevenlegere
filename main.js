ff558587ae4c945a884eb926c54c089328408858
stevenlegere <mailtostevenmarklegere@gmail.com>
2023-04-07 13:29:48 +0100

    two working statements that return the amount of buses required.

--- main.js
+++ main.js
@@ -3,3 +3,26 @@
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+// A bus can hold twentyfive passengers, allowing twentyfive people to travel around. Given i number of people, return how many buses are needed to seat everyone comfortably.
+
+// For example, howManyBuses(25) should return 1, and howManyBuses(51) should return 3.
+
+/*function howManyBuses(i) {
+    const howManyBuses = 25; // 25 people per bus (i=25)
+    const totalBuses = Math.ceil(i / howManyBuses); // Math.ceil rounds up to the nearest whole number
+
+    return totalBuses;
+};
+console.log(howManyBuses(52));*/
+
+// Party Buses.
+// A party bus can hold 25 passengers, allowing 25 people to travel. Given (numberOfPeople), return how many buses are needed to seat everyone.
+function howManyBuses(numberOfPeople)   {
+    const busCapacity = 25; // 25 people per bus
+    return Math.ceil(numberOfPeople / busCapacity); // Math.ceil rounds up to the nearest whole number
+
+}
+console.log(howManyBuses(60));
+console.log(howManyBuses(108));
+console.log(howManyBuses(248));
